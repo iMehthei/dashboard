ba46f5df0856a5cef3d3a5f82f1b752f0a9cdc21
imehthei <imehthei@gmail.com>
2025-11-17 21:55:00 +0330

    Fetch Error

--- app/dashboard/gallery/page.tsx
+++ app/dashboard/gallery/page.tsx
@@ -5,13 +5,14 @@ import { Suspense } from "react";
 import GalleryPagination from "@/app/ui/gallery/pagination";
 import { GalleryCardsSkeleton, PaginationSkeleton } from "@/app/ui/skeletons";
 
-export default async function GalleryPage({
-  searchParams,
-}: {
-  searchParams: { page?: string };
+export default async function GalleryPage(props: {
+  searchParams?: Promise<{
+    page?: string;
+  }>;
 }) {
 
-  const page = Number(searchParams?.page) || 1;
+  const searchParams = await props.searchParams;
+  const page = Number(searchParams?.page) || 1
   const limit = 12;
 
   return (
